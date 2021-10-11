18a36c7d213f6a1f1c8925df47f3aee1a339ad68
Prakash Kumar Radhakrishnan <prakash@clumio.com>
2021-10-12 08:48:34 +1100

    fix to issues pointed out by shellcheck

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$1
-if [ -z ${VERSION} ]; then
+if [ -z "${VERSION}" ]; then
   echo "Usage: ./installer.sh <version>. An example of <version> would be v0.1.0."
   exit 1
 fi
@@ -27,7 +27,7 @@ esac
 
 VERSION_NUMBER=${VERSION:1}
 TF_PLUGIN_DIR="${HOME}/.terraform.d/plugins/clumio.com/providers/clumio/${VERSION_NUMBER}/${OS}_${ARCH}"
-if ! mkdir -p ${TF_PLUGIN_DIR}; then
+if ! mkdir -p "${TF_PLUGIN_DIR}"; then
    echo "Error creating directory ${TF_PLUGIN_DIR}. Exiting..."
   exit 1
 fi
@@ -35,40 +35,51 @@ fi
 PROVIDER_NAME=terraform-provider-clumio_${VERSION_NUMBER}_${OS}_${ARCH}
 BINARY="https://github.com/clumio-code/terraform-provider-clumio/releases/download/${VERSION}/${PROVIDER_NAME}.zip"
 
-if ! mkdir ${PROVIDER_NAME}; then
+if ! mkdir "${PROVIDER_NAME}"; then
   echo "Error creating directory ${PROVIDER_NAME}. Exiting..."
   exit 1
 fi
 
-cd ${PROVIDER_NAME}
+if ! cd "${PROVIDER_NAME}"; then
+  echo "Error changing directory to ${PROVIDER_NAME}. Exiting..."
+  exit 1
+fi
 
 if command -v curl &> /dev/null; then
-  if ! curl -sSL ${BINARY} -o "${PROVIDER_NAME}.zip"; then
+  if ! curl -sSL "${BINARY}" -o "${PROVIDER_NAME}.zip"; then
     echo "Error downloading ${BINARY}. Exiting..."
     exit 1
   fi
 elif command -v wget &> /dev/null; then
-  if ! wget -q ${BINARY}; then
+  if ! wget -q "${BINARY}"; then
     echo "Error downloading ${BINARY}. Exiting..."
     exit 1
   fi
-else:
+else
   echo "wget or curl is required to download the binary. Exiting..."
   exit 1
 fi
 
-if ! unzip -q ${PROVIDER_NAME}.zip; then
+if ! unzip -q "${PROVIDER_NAME}.zip"; then
   echo "Error unzipping ${PROVIDER_NAME}.zip. Exiting..."
   exit 1
 fi
 
-if ! cp terraform-provider-clumio_${VERSION} ${TF_PLUGIN_DIR}; then
+if ! cp "terraform-provider-clumio_${VERSION}" "${TF_PLUGIN_DIR}"; then
   echo "Error copying terraform-provider-clumio_${VERSION} to ${TF_PLUGIN_DIR}. Exiting..."
   exit 1
 fi
-cd ..
+
+if ! cd ..; then
+  echo "cd .. failed. Exiting..."
+  exit 1
+fi
+
 # cleanup downloaded files
-rm -rf ${PROVIDER_NAME}*
-if [ -f ${TF_PLUGIN_DIR}/terraform-provider-clumio_${VERSION} ]; then
+if [ -d "${PROVIDER_NAME}" ]; then
+  rm -rf "${PROVIDER_NAME}"
+fi
+
+if [ -f "${TF_PLUGIN_DIR}/terraform-provider-clumio_${VERSION}" ]; then
   echo "Clumio Terraform Provider installed successfully."
 fi
